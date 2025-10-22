77e138a1103ae9d65de038810f02ac4215a5e79d
Elemmir <metsuki-sutemi@mail.ru>
2025-10-22 12:50:26 +0300

    final fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, HashRouter} from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
@@ -21,30 +21,32 @@ function App() {
   const [theme, colorMode] = useMode();
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <div className="app">
-          <Sidebar/>
-          <main className='content'>
-            <Topbar/>
-            <Routes>
-              <Route path="/" element={<Dashboard/>} />
-              <Route path="/team" element={<Team/>} />
-              <Route path="/contacts" element={<Contacts/>} />
-              <Route path="/invoices" element={<Invoices/>} />
-              <Route path="/form" element={<Form/>} />
-              <Route path="/calendar" element={<Calendar/>} />
-              <Route path="/faq" element={<FAQ/>} />
-              <Route path="/bar" element={<Bar/>} />
-              <Route path="/pie" element={<Pie/>} />
-              <Route path="/line" element={<Line/>} />
-              <Route path="/geography" element={<Geography/>} />
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <HashRouter>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <Sidebar />
+            <main className="content">
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/geography" element={<Geography />} />
+              </Routes>
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </HashRouter>
   );
 }
 
