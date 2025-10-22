d46580a6dfcc7d3a0a5c14a7937c23c5fbb01b6a
Elemmir <metsuki-sutemi@mail.ru>
2025-10-22 13:29:21 +0300

    fixed header at dasboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -20,7 +20,7 @@ const Dashboard = () => {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        {/* <Header title="DASHBOARD" subtitle="Welcome to your dashboard"></Header> */}
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard"></Header>
 
         <Box>
           <Button
